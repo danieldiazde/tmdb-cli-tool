05eb9cfdcffb22899796cc7d39b020d0812db9fd
danieldiazde <daniel.diaz.deleon@outlook.com>
2025-09-30 09:33:31 -0600

    Fix fetch_data

--- tmdb_api.py
+++ tmdb_api.py
@@ -21,12 +21,25 @@ params = {
     'language': 'en-US',
     'page': '1'
 }
+
+specifications = {'playing', 'popular', 'top', 'upcoming'}
     
-def get_now_playing_movies():
+def fetch_data(specification):
     if not ACCESS_TOKEN:
         print('Error: TMDB ACCESS TOKEN not found, please check your .env file')
         return None
-    endpoint = f'{base_url}/now_playing'
+    if specification not in specifications:
+        return None
+    if specification == 'playing':
+        path = '/now_playing'
+    elif specification == 'popular':
+        path = '/popular'
+    elif specification == 'top':
+        path = '/top_rated'
+    elif specification == 'upcoming':
+        path = '/upcoming'
+
+    endpoint = base_url+path
 
     try:
         response = requests.get(url=endpoint, params=params, headers= headers)
@@ -35,20 +48,3 @@ def get_now_playing_movies():
     except requests.exceptions.RequestException as e:
         print(f'An error ocurred: {e}')
     return None
-
-
-print(get_now_playing_movies())
-
-
-
-def get_popular_movies():
-    endpoint = f'{base_url}/popular'
-    pass
-
-def get_top_movies():
-    endpoint = f'{base_url}/top_rated'
-    pass
-
-def get_upcoming_movies():
-    endpoint = f'{base_url}/upcoming'
-    pass
