ea986f19b0e88fb71f47902c73d2554c3ea795fe
danieldiazde <daniel.diaz.deleon@outlook.com>
2025-09-30 15:17:44 -0600

    Dates work in fetch_data

--- tmdb_api.py
+++ tmdb_api.py
@@ -1,21 +1,28 @@
 import requests
 import os
 from dotenv import load_dotenv
-from datetime import datetime, timedelta
+from datetime import date, timedelta
+
+today = date.today()
+
+max_date_playing = (today+timedelta(weeks=1)).strftime('%Y-%m-%d')
+min_date_playing = (today-timedelta(weeks=3)).strftime('%Y-%m-%d')
+
+max_date_upcoming = (today+timedelta(weeks=5)).strftime('%Y-%m-%d')
+min_date_upcoming = (today+timedelta(weeks=1)).strftime('%Y-%m-%d')
 
 #Loads variables stored in dotenv
 load_dotenv()
-
 ACCESS_TOKEN = os.getenv('TMDB_ACCESS_TOKEN')
 
-base_url = 'https://api.themoviedb.org/3/movie'
+base_url = 'https://api.themoviedb.org/3'
 
 headers = {
     'Authorization': f'Bearer {ACCESS_TOKEN}',
     'accept': 'application/json'
 }
 
-params = {
+base_params = {
     'include_adult': 'false', 
     'include_video': 'false', 
     'language': 'en-US',
@@ -24,27 +31,38 @@ params = {
 
 specifications = {'playing', 'popular', 'top', 'upcoming'}
     
-def fetch_data(specification):
+def fetch_data(specification: str) -> dict:
     if not ACCESS_TOKEN:
         print('Error: TMDB ACCESS TOKEN not found, please check your .env file')
         return None
     if specification not in specifications:
+        print(f'Specification {specification} is not valid')
         return None
+    
+    local_params = base_params.copy()
+
     if specification == 'playing':
-        path = '/now_playing'
+        path = '/discover/movie'
+        local_params['release_date.gte'] = min_date_playing
+        local_params['release_date.lte'] = max_date_playing
+
     elif specification == 'popular':
-        path = '/popular'
+        path = '/movie/popular'
+
     elif specification == 'top':
-        path = '/top_rated'
+        path = '/movie/top_rated'
+
     elif specification == 'upcoming':
-        path = '/upcoming'
+        local_params['release_date.gte'] = min_date_upcoming
+        local_params['release_date.lte'] = max_date_upcoming
+        path = '/discover/movie'
 
     endpoint = base_url+path
 
     try:
-        response = requests.get(url=endpoint, params=params, headers= headers)
+        response = requests.get(url=endpoint, params=local_params, headers= headers)
         response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as e:
         print(f'An error ocurred: {e}')
-    return None
+        return None
