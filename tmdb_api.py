c172c245a166f9e49a0b430a49c9e50d6d7cee9a
danieldiazde <daniel.diaz.deleon@outlook.com>
2025-09-29 18:32:09 -0600

    First 200 response from API

--- tmdb_api.py
+++ tmdb_api.py
@@ -1,38 +1,45 @@
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime, timedelta
 
 #Loads variables stored in dotenv
 load_dotenv()
 
 ACCESS_TOKEN = os.getenv('TMDB_ACCESS_TOKEN')
 
-base_url = 'https://api.themoviedb.org/3/discover/movie'
+base_url = 'https://api.themoviedb.org/3/movie'
 
 headers = {
     'Authorization': f'Bearer {ACCESS_TOKEN}',
     'accept': 'application/json'
 }
- 
+
 params = {
     'include_adult': 'false', 
-    'include_video': 'false',
+    'include_video': 'false', 
     'language': 'en-US',
-    'page': 1,
-    'sort_by': 'popularity'
+    'page': '1'
 }
-
-def get_request(params, headers):
-    try:
-        response = requests.get(url=base_url, params=params, headers=headers)
-        if response == 200:
-            return response.json()
-    except requests.exceptions.RequestException:
-        return
     
 def get_now_playing_movies():
+    if not ACCESS_TOKEN:
+        print('Error: TMDB ACCESS TOKEN not found, please check your .env file')
+        return None
     endpoint = f'{base_url}/now_playing'
-    pass
+
+    try:
+        response = requests.get(url=endpoint, params=params, headers= headers)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f'An error ocurred: {e}')
+    return None
+
+
+print(get_now_playing_movies())
+
+
 
 def get_popular_movies():
     endpoint = f'{base_url}/popular'
