9229bdee32d11a8b144da83a8682b509af4faeaa
danieldiazde <daniel.diaz.deleon@outlook.com>
2025-09-30 21:48:55 -0600

    format_response done

--- display.py
+++ display.py
@@ -1,2 +1,12 @@
+from tmdb_api import fetch_data
 
-
+def format_response(response: dict) -> str:
+    results = response.get('results', None)
+    if results:
+        counter = 1
+        for movie in results:
+            movie_title = movie.get('title')
+            overview = movie.get('overview')
+            release_date = movie.get('release_date')
+            print(f'\n{counter}. "{movie_title}" ({release_date}):{overview[:200]}...\n')
+            counter+=1
